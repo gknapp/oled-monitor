306d5b6e5a0d9966a461eee176aefc1909182d28
Greg Knapp <hello@gregk.me>
2023-05-01 12:13:24 +0100

    Reorder functions

--- screen.py
+++ screen.py
@@ -8,23 +8,12 @@ class dotdict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-large  = ImageFont.truetype("fonts/pixeloperator.ttf", 15)
+bold   = ImageFont.truetype("fonts/pixeloperator-sc-bold.ttf", 15)
+# large  = ImageFont.truetype("fonts/pixeloperator.ttf", 15)
 medium = ImageFont.truetype("fonts/pixelmix.ttf", 8)
 small  = ImageFont.truetype("fonts/pixelade.ttf", 12)
 
-def _textwhite(draw, font):
-    return lambda pos, msg: draw.text(pos, msg, fill="white", font=font)
-
-def get_text(draw):
-    return dotdict({
-        "large": _textwhite(draw, large),
-        "write": _textwhite(draw, medium),
-        "small": _textwhite(draw, small)
-    })
-
-def get_device(sda_addr, i2c_port):
-    serial = i2c(address=sda_addr, port=i2c_port)
-    return sh1106(serial)
+# Internal module helper functions
 
 def _bar(draw, pos):
     draw.rectangle(pos, outline="white", fill="white")
@@ -36,9 +25,35 @@ def _inset(pos, px):
     x, y, xmax, ymax = pos
     return (x + px, y + px, xmax - px, ymax - px)
 
+def _textwhite(draw, font):
+    return lambda pos, msg: draw.text(pos, msg, fill="white", font=font)
+
+# Public functions
+
 def bar_gauge(draw, pos, perc = 0):
     (x, y, xmax, ymax) = _inset(pos, 2)
     max = xmax - x
     width = round(perc / 100 * max)
     _frame(draw, pos)
     _bar(draw, (x, y, x + width, ymax))
+
+def get_device(sda_addr, i2c_port):
+    serial = i2c(address=sda_addr, port=i2c_port)
+    return sh1106(serial)
+
+def get_gauge(draw, text):
+    def gauge(pos, label, percent):
+        (x, y) = pos
+        bar_gauge(draw, (x + 21, y, x + 96, y + 8), percent)
+        text.small((x, y - 2), label)
+        text.small((x + 99, y - 2), str(round(percent, 1)) + "%")
+    
+    return gauge
+
+def get_text(draw):
+    return dotdict({
+        "bold": _textwhite(draw, bold),
+        # "large": _textwhite(draw, large),
+        "small": _textwhite(draw, small),
+        "write": _textwhite(draw, medium)
+    })
