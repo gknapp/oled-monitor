be8453e27e09fc1d0f851266776d4354b305d7a3
Greg Knapp <hello@gregk.me>
2023-05-07 12:53:13 +0100

    Add --reinstall-venv switch, install required apt packages
    Install required system packages: python3-virtualenv, virtualenv
    Refactor script into functions
    Update systemd service, After=network.target
    Add coloured output for successful and failed results

--- install.sh
+++ install.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
-if [ ! -d ".env" ]; then
+
+MAIN_SCRIPT="oled-monitor.py"
+PACKAGES="python3-virtualenv virtualenv"
+SYSD_SVC="oled-monitor.service"
+SYSD_FILE="/lib/systemd/system/$SYSD_SVC"
+
+std='\033[0m'
+
+echo_ok() {
+    local green='\033[0;32m'
+    printf "${green}$1${std}\n"
+}
+
+echo_error() {
+    local red='\033[0;31m'
+    printf "${red}$1${std}\n"
+}
+
+check_packages() {
+    local deps=$(echo $PACKAGES | tr ' ' '|')
+    local installed=$(apt list --installed 2>/dev/null | grep -E "^$deps" | wc -l)
+    echo $installed
+}
+
+ensure_executable() {
+    if [[ ! -x $MAIN_SCRIPT ]]; then
+        chmod u+x,o-x $MAIN_SCRIPT
+    fi
+}
+
+install_packages() {
+    echo "Installing required system packages ..."
+    apt update
+    apt install -y $PACKAGES
+}
+
+install_python_env() {
     echo "Creating virtual environment ..."
     python3 -m venv .env && source .env/bin/activate
     
@@ -7,64 +43,114 @@ if [ ! -d ".env" ]; then
         echo "Installing dependencies ..."
         pip install -r requirements.txt
     else
-        echo "Failed to create virtual environment. Install aborted."
+        echo "Failed to create virtual environment."
+        echo_error "Install aborted"
         exit 1
     fi
-fi
+}
 
-if [[ ! -x "./oled-monitor.py" ]]; then
-    chmod u+x,o-x ./oled-monitor.py
-fi
+remove_python_env() {
+    rm -rf .env
+    echo "Removed virtual environment"
+}
 
-if [ ! "$1" == "--install-service" ]; then
+install_service_prompt() {
+    echo ""
     read -p "Start OLED Monitor automatically on boot? [Y/n] " -n 1 -r
 
     if [[ ! $REPLY =~ ^[Yy]$ ]] && [[ ! "$REPLY" == "" ]]; then
-        echo ""
-        echo "Installation complete."
-        exit
+        echo 0
+    else
+        echo 1
     fi
+}
 
-    [ "$(id -u)" -ne 0 ] && exec sudo -- "$0" --install-service
-fi
+install_service() {
+    echo "Installing OLED monitor to run at system start ..."
 
-SYSD_SVC="oled-monitor.service"
-SYSD_FILE="/lib/systemd/system/$SYSD_SVC"
-
-echo "Installing OLED monitor to run at system start ..."
-
-cat << EOF > $SYSD_FILE
+    cat << EOF > $SYSD_FILE
 [Unit]
 Description=OLED system resource service
-After=multi-user.target
+After=network.target
 
 [Service]
-ExecStart=$(pwd)/.env/bin/python $(pwd)/oled-monitor.py
+ExecStart=$(pwd)/.env/bin/python $(pwd)/$MAIN_SCRIPT
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl enable $SYSD_SVC
+    systemctl daemon-reload
+    systemctl enable $SYSD_SVC
+}
 
-echo -n "Starting service ... "
+is_root() {
+    if [ "$(id -u)" -eq 0 ]; then echo "y"; else echo "n"; fi
+}
 
-systemctl start $SYSD_SVC
+start_service() {
+    echo -n "Starting service ... "
+    systemctl start $SYSD_SVC
 
-if [ "$?" -eq 0 ]; then
-    echo "done."
-    echo "Installation successful!"
-    echo ""
-    echo "You can check the service status with:"
-    echo "  sudo systemctl status $SYSD_SVC"
-    echo ""
-    echo "To uninstall:"
-    echo "  sudo systemctl disable $SYSD_SVC"
-    echo "  sudo systemctl stop $SYSD_SVC"
-    echo ""
+    if [ "$?" -eq 0 ]; then
+        echo_ok "OK"
+        echo "Installation successful!"
+        echo ""
+        echo "You can check the service status with:"
+        echo "  sudo systemctl status $SYSD_SVC"
+        echo ""
+        echo "To uninstall:"
+        echo "  sudo systemctl disable $SYSD_SVC"
+        echo "  sudo systemctl stop $SYSD_SVC"
+        echo ""
+    else
+        echo_error "failed!"
+        echo "Service could not be started."
+    fi
+}
+
+ensure_executable
+
+if [[ ! "$1" == "--root" ]]; then # not elevated, checks not run
+    echo -n "Checking required system packages are installed ... "
+    installed=$(check_packages)
+
+    if [[ $installed -lt 2 ]]; then
+        echo "Enter password to install required packages"
+        [ $(is_root) == "n" ] && exec sudo -- "$0" --root
+    else
+        echo_ok "OK"
+    fi
+else # running with --root, suppress prompts
+    [ $(check_packages) -lt 2 ] && install_packages
+fi
+
+if [[ "$1" == "--reinstall-venv" ]]; then
+    # Force re-installation of python venv
+    if [ -d ".env" ]; then remove_python_env; fi
+    install_python_env
 else
-    echo "failed!"
-    echo "Service could not be started."
+    # Only install dependencies if virtualenv doesn't exist
+    if [ ! -d ".env" ]; then install_python_env; fi
+fi
+
+if [[ "$1" == "--root" ]]; then
+    # suppress prompt
+    install_service
+    start_service
+else
+    if [[ ! "$1" == "--reinstall-venv" && $(install_service_prompt) -eq "1" ]]; then
+        echo ""
+
+        if [ $(is_root) == "n" ]; then
+            sudo -- "$0" --root
+        else # already root
+            install_service
+            start_service
+        fi
+    else
+        echo ""
+        echo "Installation complete"
+    fi
 fi
